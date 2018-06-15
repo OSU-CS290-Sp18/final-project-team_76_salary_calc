bbff4665d67a02314b36e24b4bb62a6bffaf8993
BandyLin <38176500+BandyLin@users.noreply.github.com>
2018-06-15 07:57:30 -0700

    Delete postTemplate.js

--- public/postTemplate.js
+++
@@ -1,7 +0,0 @@
-var peopleData = document.getElementById('payData');
-var salaryPostTemplate = Handlebars.compile(salaryPost.handlebars);
-
-Handlebars.registerHelper(payData, function(text){
-  text = Handlebars.Utils.excapeExpression(text);
-  retrun new Handlebars.SafeString(text);
-});
